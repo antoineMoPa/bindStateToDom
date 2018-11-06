9cb58d8dc1d83de0a25c89154742c9a6c3e59ebb
Antoine <antoine.morin.paulhus@gmail.com>
2018-11-06 16:02:23 -0500

    Array push support

--- script.js
+++ script.js
@@ -10,6 +10,36 @@
 function bindStateToDom(state, element, options){
   let components = {};
 
+  /*
+     Gets path to object bla.bla in node attribute
+     and transform to array ['bla', 'bla']
+     Replaces any found alias (ex $i -> 0)
+     
+     Returns false if some aliases can't be resolved
+  */
+  let replaceAliases = (attribute, node) => {
+    let name = node.getAttribute(attribute).split(".");
+    
+    // Replace aliases, such as iterators
+    for(let i = 0; i < name.length; i++) {
+      if(name[i][0] == '$'){
+        // Name starts with $, so it is a variable
+        // Take the rest of the string as it is the variable name
+        let variable = name[i].substr(1);
+        let value = node.getAttribute("data-alias-" + variable);
+        
+        if(value == null) {
+          // No alias found
+          return false;
+        }
+        name[i] = value;
+      }
+    }
+    
+    return name;
+  };
+
+  // Some default components
   components["INPUT"] = {
     domValueGetter: (el) => ( el.value ),
     domValueSetter: (el, newValue) => ( el.value = newValue )
@@ -19,10 +49,32 @@ function bindStateToDom(state, element, options){
     domValueGetter: (el) => ( el.innerText ),
     domValueSetter: (el, newValue) => ( el.innerText = newValue )
   };
+
+  components["APPEND-BUTTON"] = {
+    domClick: (el, state, subState) => {
+      /* 
+        Here we choose to add null because it works with both
+        number and text inputs.
+        You could choose to add a parameter data-default-value
+        Or data-default-json for arrays of objects
+        To do this, you can override this component or create a new tag
+      */
+      var name = replaceAliases("data-map-to", el);
+      subState._append(null, "");
+    }
+  };
+
+  components["DELETE-BUTTON"] = {
+    domClick: (el, state) => {
+      var name = replaceAliases("data-map-to", el);
+      state._delete(name[name.length-1]);
+    }
+  };
   
   // Some components require the same api
   components["SPAN"] = components["P"];
-  
+
+  // Load user components
   if(options){
     if(options.extraComponents){
       for(let i in options.extraComponents){
@@ -31,7 +83,9 @@ function bindStateToDom(state, element, options){
     }
   }
   
-  // Add getters, setters and value to state
+  /*
+    Add getters, setters, updaters and value to state
+  */
   let prepareState = (state) => {
     for(let i in state){
       if(typeof(state[i]._initialized) != "undefined"){
@@ -46,8 +100,58 @@ function bindStateToDom(state, element, options){
       if (typeof(state[i]) === "object") {
         // Note: arrays are also objects
 
+        // This method is called when a child is updated
+        // So we can perform actions when a part of a subTree is updated
         state[i]._set = (subState) => {
           state[i]._updaters.map((fn) => fn(subState));
+        };
+
+        // This method can be used to safely delete an element
+        state[i]._delete = (item) => {
+          item = parseInt(item);
+          // Call dom deleter for item if it exists
+          state[i][item]._domDeleters.map( (fn) => fn() );
+          
+          state[i].splice(item, 1);
+        };
+
+        // This will keep a list of functions to call
+        // When adding elements to an array
+        state[i]._domAppenders = [];
+        
+        /*
+          This method can be used to add an `value` at the end
+          of an array, updating the dom.
+          
+          if item is null, the row is pushed at the end
+          else, we insert at position `item`
+          
+          if value is undefined, we assume index was null
+          and use `index` as value that we push at the end of array
+        */
+        state[i]._append = (index, value) => {
+          if(typeof(value) == "undefined"){
+            value = index;
+            index = null;
+          }
+          
+          if(index == null){
+            // Append at end if no position was given
+            state[i].push(value);
+            index = state[i].length - 1;
+          } else {
+            index = parseInt(index);
+            state[i].splice(index, 1, value);
+          }
+          
+          // Prepare added part of state
+          prepareState(state[i]);
+
+          // Call functions that create the dom elements
+          state[i]._domAppenders.map( (fn) => fn(state[i], index) );
+          // Call updaters of parent object
+          state[i]._set();
+          
         };
         
         // Recurse within object
@@ -57,7 +161,10 @@ function bindStateToDom(state, element, options){
         let value = state[i];
 
         state[i] = {};
-        
+        // This will keep a list of functions to call
+        // When deleting an object
+        state[i]._domDeleters = [];
+        // The access point to modify and get values
         state[i]._get = () => ( state[i]._value_do_not_modify );
         state[i]._set = (newValue) => {
           state[i]._updaters.map((fn) => fn(newValue));
@@ -65,24 +172,36 @@ function bindStateToDom(state, element, options){
           // Call parent updaters
           state._set(state);
         };
+        // This is where we store the state
+        // Updating it directly will just mess stuff
         state[i]._value_do_not_modify = value;
       }
       
       // Updaters will be called upon setting value
       state[i]._updaters = [];
-      
+
+      // Mark this element as done
       state[i]._initialized = true;
     }
   };
   
   prepareState(state);
   
-  let elements = element.querySelectorAll(":scope, [data-map-to]:not([data-map-to*=\\$])");
-  
+  /*
+     Look for data-map-to in dom and bind it
+  */
+  let elements = element.querySelectorAll(":scope, [data-map-to]");
   let initMapTo = ((el) => {
-    // Find path to object bla.bla
-    // and transform to array ['bla', 'bla']
-    let name = el.getAttribute("data-map-to").split(".");
+    let name = replaceAliases("data-map-to", el);
+    
+    if(name === false){
+      // Element contains a variable that has not been set yet.
+      // Most of the time, this is a loop element template
+      // We could potentially detect if rendering should be finished
+      // And throw an error, if lot's of bug end up here.
+      return;
+    }
+
     let currentStateObject = null;
     let objCursor = state;
     
@@ -91,7 +210,8 @@ function bindStateToDom(state, element, options){
     for(var i = 0; i < name.length - 1; i++) {
       objCursor = objCursor[name[i]];
     }
-    
+
+    let parentObject = objCursor;
     currentStateObject = objCursor[name[i]];
 
     if(typeof(currentStateObject) == "undefined") {
@@ -105,14 +225,23 @@ function bindStateToDom(state, element, options){
       currentStateObject._set(newVal);
     };
 
+    // This method will update the state on component change
+    let clickListener = (event) => {
+      let newVal = el.domClick(el, parentObject, currentStateObject);
+    };
+    
     // Bind listener to common input events
     el.onchange = domListener;
     el.onkeyup = domListener;
-
+    el.onclick = clickListener;
+    
     // Set DOM accessors
     if(typeof(components[el.nodeName]) != "undefined"){
-      el.domValueGetter = components[el.nodeName].domValueGetter;
-      el.domValueSetter = components[el.nodeName].domValueSetter;
+      let nullAction = () => {};
+      // Bind method found in appropriate component
+      el.domValueGetter = components[el.nodeName].domValueGetter || nullAction;
+      el.domValueSetter = components[el.nodeName].domValueSetter || nullAction;
+      el.domClick = components[el.nodeName].domClick || nullAction;
     } else {
       console.error("No component defined for '"+el.nodeName+"'");
     }
@@ -129,15 +258,16 @@ function bindStateToDom(state, element, options){
     }
   });
 
+  // Initalize data-map-to for root node and sub nodes
   elements.forEach(initMapTo);
-  // Initalize data-map-to for root node
   if(element.getAttribute("data-map-to") != null) {
     initMapTo(element);
   }
   
-  // Enable for loops
+  /*
+    Initialize loops
+  */
   let loops = element.querySelectorAll("[data-loop]");
-
   loops.forEach((el) => {
     // Split argument
     let loopParts = el.getAttribute("data-loop").split(" in ");
@@ -167,17 +297,18 @@ function bindStateToDom(state, element, options){
 
     if(Array.isArray(currentStateObject)){
       let container = document.createElement(el.nodeName);
-      
-      for(let i in currentStateObject) {
+
+      let domAppender = (currentStateObject, i) => {
         // Skip our control variables
         if(i[0] == '_') {
-          continue;
+          return;
         }
-        
+
         let domRow = document.createElement(el.nodeName);
         // Clone content
         domRow.innerHTML = el.innerHTML;
         
+          
         // Copy all attributes (classes, styles) to rows
         for(let attribute of el.getAttributeNames()){
           // ( Except attribute data-loop )
@@ -186,51 +317,57 @@ function bindStateToDom(state, element, options){
           }
         }
 
+        domRow.style.display = null;
+        
         // Replace $i by iterator value
         let nodes = domRow.querySelectorAll("[data-map-to*=\\$"+iterator+"]");
         
         let replaceAttributes = (node) => {
-          let newAttribute = node.getAttribute("data-map-to");
-          newAttribute = newAttribute.replace("$"+iterator, i);
-          node.setAttribute("data-map-to", newAttribute);
+          node.setAttribute("data-alias-"+iterator, i);
         };
-
+        
+        // Change aliases for each sub nodes
         nodes.forEach(replaceAttributes);
-
+        
+        // Perform deletion
+        currentStateObject[i]._domDeleters.push(() => {
+          // Decrement next iterators
+          // Replace $i by iterator value
+          let nodes = domRow.parentNode.querySelectorAll("[data-map-to*=\\$"+iterator+"]");
+          
+          let replaceAttributes = (node) => {
+            let value = parseInt(node.getAttribute("data-alias-" + iterator));
+            if(value >= i){
+              node.setAttribute("data-alias-"+iterator, value-1);
+            }
+          };
+          
+          // Change aliases for each sub nodes
+          nodes.forEach(replaceAttributes);
+          
+          domRow.parentNode.removeChild(domRow);
+        });
+        
         // Replace iterator in root node
         if(domRow.getAttribute("data-map-to") != null){
           replaceAttributes(domRow);
         }
-
+        
         bindStateToDom(state, domRow);
         container.appendChild(domRow);
+      };
+
+      // Append all objects already in state
+      for(let i in currentStateObject) {
+        domAppender(currentStateObject, i);
       }
       
+      // Elements created after page load will also use the domAppender
+      currentStateObject._domAppenders.push(domAppender);
+
       // Hide original element
       el.style.display = "none";
       el.parentNode.insertBefore(container, el);
     }
   });
 }
-
-const state = {
-  meal: {
-    vegetable: "Potato",
-    vegetableCount: 42,
-    fruits: [
-      "apple",
-      "tomato",
-      "orange"
-    ]
-  },
-  lines_of_code: '?'
-};
-
-const extraComponents = {
-  "potatoout": {
-    domValueGetter: (el) => ( el.innerText ),
-    domValueSetter: (el, newValue) => ( el.innerText = "🥔" + newValue + "🥔" )
-  }
-};
-
-bindStateToDom(state, document.body, {extraComponents: extraComponents});
